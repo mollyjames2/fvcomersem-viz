cb718ed3154587cc5874029b3df3951668e4d22c
Molly James <moja@pml.ac.uk>
2025-10-23 10:33:42 +0100

    build: adopt setuptools-scm; ci: add publish-on-tag workflow

--- src/fvcomersemviz/__init__.py
+++ src/fvcomersemviz/__init__.py
@@ -1,2 +1,12 @@
-__all__ = ['__version__']
-__version__ = '0.0.1'
+try:
+    from ._version import version as __version__  # written by setuptools-scm at build time
+except Exception:
+    try:
+        from importlib.metadata import version, PackageNotFoundError
+    except ImportError:  # for very old Pythons (not expected)
+        from importlib_metadata import version, PackageNotFoundError  # type: ignore
+    try:
+        __version__ = version("fvcomersem-viz")
+    except PackageNotFoundError:
+        __version__ = "0+unknown"
+
