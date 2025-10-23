30ce3f85c8b922ea7e9b28d240691ac603c74a2c
Molly James <moja@pml.ac.uk>
2025-10-23 18:06:34 +0100

    Static versioning (0.0.1); no tags; minimal packaging

--- src/fvcomersemviz/__init__.py
+++ src/fvcomersemviz/__init__.py
@@ -1,12 +1,2 @@
-try:
-    from ._version import version as __version__  # written by setuptools-scm at build time
-except Exception:
-    try:
-        from importlib.metadata import version, PackageNotFoundError
-    except ImportError:  # for very old Pythons (not expected)
-        from importlib_metadata import version, PackageNotFoundError  # type: ignore
-    try:
-        __version__ = version("fvcomersemviz")
-    except PackageNotFoundError:
-        __version__ = "0+unknown"
+__version__ = "0.0.1"
 
